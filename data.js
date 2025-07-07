90686f180b6048b8b73108b219054b12fce63c94
AIMI-1 <muhammadalyandi1@gmail.com>
2025-07-08 01:36:06 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,15 @@
-
 const pelanggan = [
-  { nama: "Budi", lat: -6.2, lon: 106.8, marketing: "Lusi" },
-  { nama: "Tini", lat: -7.2, lon: 112.7, marketing: "Erika" },
-  { nama: "Andi", lat: -5.1, lon: 119.4, marketing: "Mita" },
-  { nama: "Dewi", lat: -6.9, lon: 107.6, marketing: "Lyn" },
-  { nama: "Rina", lat: -3.3, lon: 114.6, marketing: "Chilly" },
-  { nama: "Joko", lat: -0.9, lon: 100.4, marketing: "Ferdy" },
+  { nama: "Toko A", marketing: "Lusi", lat: -6.2, lon: 106.8, kota: "Jakarta Selatan" },
+  { nama: "Toko B", marketing: "Mita", lat: -7.25, lon: 112.75, kota: "Surabaya" },
+  { nama: "Toko C", marketing: "Ferdy", lat: -6.9, lon: 107.6, kota: "Bandung" },
+  { nama: "Toko D", marketing: "Erika", lat: -6.3, lon: 106.9, kota: "Bekasi" }
 ];
+
+const wilayahIndonesia = {
+  "DKI Jakarta": ["Jakarta Selatan", "Jakarta Barat", "Jakarta Timur"],
+  "Jawa Barat": ["Bandung", "Bekasi", "Bogor", "Depok"],
+  "Jawa Timur": ["Surabaya", "Malang", "Sidoarjo"],
+  "Jawa Tengah": ["Semarang", "Solo", "Magelang"]
+};
+
+const kotaDenganPelanggan = pelanggan.map(p => p.kota);
