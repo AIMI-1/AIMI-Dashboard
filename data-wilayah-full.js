3e0c7d0545c3314e77cd2f46e311be270aaee949
AIMI-1 <muhammadalyandi1@gmail.com>
2025-07-08 00:44:02 +0700

    Add files via upload

---
+++ data-wilayah-full.js
@@ -0,0 +1,159 @@
+const wilayahIndonesia = {
+  "Aceh": [
+    "Kabupaten Aceh Barat",
+    "Kabupaten Aceh Besar",
+    "Kota Banda Aceh"
+  ],
+  "Sumatera Utara": [
+    "Kabupaten Deli Serdang",
+    "Kota Medan",
+    "Kota Binjai"
+  ],
+  "Sumatera Barat": [
+    "Kabupaten Agam",
+    "Kota Padang",
+    "Kota Bukittinggi"
+  ],
+  "Riau": [
+    "Kabupaten Bengkalis",
+    "Kota Pekanbaru"
+  ],
+  "Jambi": [
+    "Kabupaten Bungo",
+    "Kota Jambi"
+  ],
+  "Sumatera Selatan": [
+    "Kabupaten Musi Banyuasin",
+    "Kota Palembang"
+  ],
+  "Bengkulu": [
+    "Kabupaten Seluma",
+    "Kota Bengkulu"
+  ],
+  "Lampung": [
+    "Kabupaten Lampung Selatan",
+    "Kota Bandar Lampung"
+  ],
+  "Kepulauan Bangka Belitung": [
+    "Kabupaten Bangka",
+    "Kota Pangkalpinang"
+  ],
+  "Kepulauan Riau": [
+    "Kabupaten Bintan",
+    "Kota Batam",
+    "Kota Tanjungpinang"
+  ],
+  "DKI Jakarta": [
+    "Kota Jakarta Barat",
+    "Kota Jakarta Selatan",
+    "Kabupaten Kepulauan Seribu"
+  ],
+  "Jawa Barat": [
+    "Kabupaten Bandung",
+    "Kota Bekasi",
+    "Kota Bandung",
+    "Kota Bogor"
+  ],
+  "Jawa Tengah": [
+    "Kabupaten Banyumas",
+    "Kota Semarang",
+    "Kota Surakarta"
+  ],
+  "DI Yogyakarta": [
+    "Kabupaten Sleman",
+    "Kota Yogyakarta"
+  ],
+  "Jawa Timur": [
+    "Kabupaten Sidoarjo",
+    "Kota Surabaya",
+    "Kota Malang"
+  ],
+  "Banten": [
+    "Kabupaten Tangerang",
+    "Kota Tangerang",
+    "Kota Cilegon"
+  ],
+  "Bali": [
+    "Kabupaten Badung",
+    "Kota Denpasar"
+  ],
+  "Nusa Tenggara Barat": [
+    "Kabupaten Lombok Barat",
+    "Kota Mataram"
+  ],
+  "Nusa Tenggara Timur": [
+    "Kabupaten Kupang",
+    "Kota Kupang"
+  ],
+  "Kalimantan Barat": [
+    "Kabupaten Ketapang",
+    "Kota Pontianak"
+  ],
+  "Kalimantan Tengah": [
+    "Kabupaten Kapuas",
+    "Kota Palangka Raya"
+  ],
+  "Kalimantan Selatan": [
+    "Kabupaten Banjar",
+    "Kota Banjarmasin"
+  ],
+  "Kalimantan Timur": [
+    "Kabupaten Kutai Kartanegara",
+    "Kota Samarinda"
+  ],
+  "Kalimantan Utara": [
+    "Kabupaten Bulungan",
+    "Kota Tarakan"
+  ],
+  "Sulawesi Utara": [
+    "Kabupaten Minahasa",
+    "Kota Manado"
+  ],
+  "Sulawesi Tengah": [
+    "Kabupaten Parigi Moutong",
+    "Kota Palu"
+  ],
+  "Sulawesi Selatan": [
+    "Kabupaten Gowa",
+    "Kota Makassar"
+  ],
+  "Sulawesi Tenggara": [
+    "Kabupaten Konawe",
+    "Kota Kendari"
+  ],
+  "Gorontalo": [
+    "Kabupaten Bone Bolango",
+    "Kota Gorontalo"
+  ],
+  "Sulawesi Barat": [
+    "Kabupaten Mamuju"
+  ],
+  "Maluku": [
+    "Kabupaten Maluku Tengah",
+    "Kota Ambon"
+  ],
+  "Maluku Utara": [
+    "Kabupaten Halmahera Barat",
+    "Kota Ternate"
+  ],
+  "Papua": [
+    "Kabupaten Jayapura",
+    "Kota Jayapura"
+  ],
+  "Papua Barat": [
+    "Kabupaten Manokwari",
+    "Kota Sorong"
+  ],
+  "Papua Selatan": [
+    "Kabupaten Merauke"
+  ],
+  "Papua Tengah": [
+    "Kabupaten Nabire"
+  ],
+  "Papua Pegunungan": [
+    "Kabupaten Jayawijaya"
+  ],
+  "Papua Barat Daya": [
+    "Kabupaten Sorong"
+  ]
+};
